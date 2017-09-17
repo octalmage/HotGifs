daafb272451e1abd396cd8097dc8628414c9ea1d
Jason Stallings <octalmage@gmail.com>
2017-09-17 15:06:27 -0500

    Pass config to main.

--- renderer.js
+++ renderer.js
@@ -4,10 +4,12 @@ const { remote, clipboard } = require('electron');
 const win = remote.getCurrentWindow();
 // Require our tags.
 require('./assets/tags/main.tag');
+require('./assets/tags/settings.tag');
 
 riot.mount('main', {
   win,
   clipboard,
   settings: win.settings,
   visitor: win.visitor,
+  config: win.config,
 });
